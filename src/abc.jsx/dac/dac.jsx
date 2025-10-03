34c31192a87014c5b6694f7efb720949dc82c049
Igihozo Belise <igihozobelise6@gmail.com>
2025-10-04 00:09:17 +0200

    add

---
+++ src/abc.jsx/dac/dac.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const dac = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default dac
---
+++ src/abc.jsx/dac/mn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const mn = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default mn
